d1e2166b2ce39a02a10624be7900416f5109b6fe
aa shulpin <aashulpin@gmail.com>
2019-08-04 07:53:57 +0500

    circle is moving in bounds on click

--- app/src/main/java/ru/you11/haabtestproject/Circle.kt
+++ app/src/main/java/ru/you11/haabtestproject/Circle.kt
@@ -3,10 +3,16 @@ package ru.you11.haabtestproject
 import android.content.Context
 import android.graphics.Canvas
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
-import androidx.core.content.ContextCompat
+import kotlin.math.*
 
-class Circle: View {
+class Circle : View {
+
+    var angle = 0.0
+
+    private val parentWidth by lazy { (parent as View).width  }
+    private val parentHeight by lazy { (parent as View).height  }
 
     constructor(context: Context) : super(context)
 
@@ -18,4 +24,49 @@ class Circle: View {
         super.onDraw(canvas)
         setBackgroundResource(R.drawable.circle_shape)
     }
+
+    fun setMoving(stepDistance: Float) {
+
+        var xDist = getXDist(stepDistance)
+        var yDist = getYDist(stepDistance)
+
+        if (isXOutOfBounds(xDist)) {
+            angle = 360 - angle
+            xDist = getXDist(stepDistance)
+        }
+
+        if (isYOutOfBounds(yDist)) {
+            angle = if (angle <= 180) {
+                180 - angle
+            } else {
+                540 - angle
+            }
+            yDist = getYDist(stepDistance)
+        }
+
+        x += xDist
+        y += yDist
+
+        log("angle: $angle")
+        log("cos: ${cos(Math.toRadians(angle))}")
+        log("sin: ${sin(Math.toRadians(angle))}")
+        log("x dist: $xDist")
+        log("y dist: $yDist")
+
+        invalidate()
+    }
+
+    private fun getXDist(stepDistance: Float) = (stepDistance * sin(Math.toRadians(angle))).toFloat()
+    private fun getYDist(stepDistance: Float) = -(stepDistance * cos(Math.toRadians(angle))).toFloat()
+
+    private fun isXOutOfBounds(moveDistance: Float) = isOutOfBounds(x, moveDistance, width, parentWidth)
+    private fun isYOutOfBounds(moveDistance: Float) = isOutOfBounds(y, moveDistance, height, parentHeight)
+
+    private fun isOutOfBounds(coordinate: Float, moveDistance: Float, viewSide: Int, parentSide: Int): Boolean =
+        coordinate + moveDistance + viewSide > parentSide || coordinate + moveDistance < 0
+
+    private fun log(message: String) {
+        Log.d("meow", message)
+    }
+
 }
--- app/src/main/java/ru/you11/haabtestproject/MainActivity.kt
+++ app/src/main/java/ru/you11/haabtestproject/MainActivity.kt
@@ -2,11 +2,17 @@ package ru.you11.haabtestproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        firstCircle.angle = 165.0
+        firstCircle.setOnClickListener {
+            firstCircle.setMoving(25f)
+        }
     }
 }
